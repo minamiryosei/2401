eea4c934013ba5764dbf5675c9edc8f69303ca3c
mai3373 <137670006+mai3373@users.noreply.github.com>
2024-01-29 13:15:22 +0900

    勤怠一覧　バック　須藤

--- 2401teamdevelopment/src/main/java/com/example/demo/controller/AttendanceListController.java
+++ 2401teamdevelopment/src/main/java/com/example/demo/controller/AttendanceListController.java
@@ -1,57 +1,38 @@
 package com.example.demo.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.AttendanceListEntity;
 import com.example.demo.service.AttendanceListService;
-
 /**
- * ユーザー情報 Controller
+ * 勤怠一覧 Controller
  */
 @Controller
 public class AttendanceListController {
 
   /**
-   * ユーザー情報 Service
+   * 勤怠一覧情報 Service
    */
   @Autowired
   private AttendanceListService attendanceListService;
 
   /**
-   * ユーザー情報一覧画面を表示
+   * 勤怠一覧画面を表示
    * @param model Model
-   * @return ユーザー情報一覧画面
+   * @return 勤怠一覧画面のHTML
    */
-  @GetMapping(value = "/attendanceList/list")
-  public String displayList(Model model) {
-    List<AttendanceList> attendanceList = AttendanceListService.searchAll();
-    model.addAttribute("attendanceList", attendanceList);
-    return "attendanceList/list";
+  @RequestMapping("/attendanceList/list")
+  public String attendanceList(Model model) {
+    List<AttendanceListEntity> attendancelist = attendanceListService.searchAll();
+    model.addAttribute("attendancelist", attendancelist);
+    return "attendancelist/list";
   }
 
-  /**
-   * ユーザー新規登録画面を表示
-   * @param model Model
-   * @return ユーザー情報一覧画面
-   */
-  @GetMapping(value = "/attendanceList/add")
-  public String displayAdd(Model model) {
-    return "attendanceList/add";
-  }
-
-  /**
-   * ユーザー情報詳細画面を表示
-   * @param id 表示するユーザーID
-   * @param model Model
-   * @return ユーザー情報詳細画面
-   */
-  @GetMapping("/attendanceList/{id}")
-  public String displayView(@PathVariable Long id, Model model) {
-    return "attendanceList/view";
-  }
+ 
 }
