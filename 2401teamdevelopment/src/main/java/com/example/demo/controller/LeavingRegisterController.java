74190436d6d4fa7bfc68acc93195cb42656e14c4
haru640 <136230611+haru640@users.noreply.github.com>
2024-02-07 12:05:30 +0900

    退勤登録　2/7

--- 2401teamdevelopment/src/main/java/com/example/demo/controller/LeavingRegisterController.java
+++ 2401teamdevelopment/src/main/java/com/example/demo/controller/LeavingRegisterController.java
@@ -1,11 +1,46 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.request.LeavingRegisterrequest;
+import com.example.demo.service.LeavingRegisterService;
 
 @Controller
 public class LeavingRegisterController {
-	
 
-	
+	@Autowired
+	public LeavingRegisterService leavingRegisterService;
 
+	@GetMapping("leavingRegister")
+	public String displayAdd(Model model) {
+		model.addAttribute("leavingRegisterRequest", new LeavingRegisterrequest());
+		return "LeavingRegister";
+	}
+
+	@PostMapping("/LeavingRegister/create")
+	public String create(@Validated @ModelAttribute LeavingRegisterrequest leavingRegisterRequest,
+			BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+			List<String> errorList = new ArrayList<String>();
+			for (ObjectError error : result.getAllErrors())
+				errorList.add(error.getDefaultMessage());
+		    }
+			model.addAttribute("ValidationError",errorList);
+			return "LeavingRegister";
+	
+        LeavingRegisterService.create(LeavingRegisterRequest);
+	    return "LeavingRegister";
+	}
 }
