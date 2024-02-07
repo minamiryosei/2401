7dc74f1419b08e617336cc585deec480bf04c7dd
haru640 <136230611+haru640@users.noreply.github.com>
2024-02-07 13:00:15 +0900

    退勤登録

---
+++ 2401teamdevelopment/src/main/java/com/example/demo/entity/LeavingRegisterentity.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table
+
+public class LeavingRegisterentity {
+
+	@Id
+	@Column(name = "attendance_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer attendance_id;
+
+	@Column(name = "user_id")
+	private Integer user_id;
+
+	@Column(name = "status")
+	private String status;
+
+	@Column(name = "leaving_date")
+	private LocalDate leaving_date;
+
+	@Column(name = "leaving_time")
+	private LocalTime leaving_time;
+
+	@Column(name = "break_time")
+	private LocalTime break_time;
+
+	@Column(name = "remarks")
+	private String remarks;
+
+}
