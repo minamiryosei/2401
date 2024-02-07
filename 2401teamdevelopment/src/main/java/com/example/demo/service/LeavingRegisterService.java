c324185253343d81594f5ab265a8291f23209b52
haru640 <136230611+haru640@users.noreply.github.com>
2024-02-07 15:36:21 +0900

    退勤登録

--- 2401teamdevelopment/src/main/java/com/example/demo/service/LeavingRegisterService.java
+++ 2401teamdevelopment/src/main/java/com/example/demo/service/LeavingRegisterService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional(dontRollbackOn =Exception.class )
 public class LeavingRegisterService {
+	
+@Autowired
+	private Leav
 
 }
