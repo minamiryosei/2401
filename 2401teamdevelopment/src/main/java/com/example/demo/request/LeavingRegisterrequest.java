bc30b87c1f18e59618addc9a0c3276ee2e22eee8
haru640 <136230611+haru640@users.noreply.github.com>
2024-02-07 15:26:02 +0900

    退勤登録

--- 2401teamdevelopment/src/main/java/com/example/demo/request/LeavingRegisterrequest.java
+++ 2401teamdevelopment/src/main/java/com/example/demo/request/LeavingRegisterrequest.java
@@ -1,7 +1,37 @@
 package com.example.demo.request;
 
-public class LeavingRegisterrequest {
-	
-	
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class LeavingRegisterrequest implements Serializable {
+
+	@NonNull(message = "ユーザーIDを入力してください")
+	private Integer user_id;
+
+	@NonNull(message = "ステータスの選択をしてください")
+	private String status;
+
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@NotNull(message = "退勤日を入力してください")
+	private LocalDate Leaving_date;
+
+	@DateTimeFormat(pattern = "HH:mm")
+	@NonNull(message = "退勤時間を入力してください")
+	private LocalTime Leaving_time;
+
+	@NotNull(message = "休憩時間を選択をしてください")
+	private String break_time;
+
+	@Length(max = 100)
+	private String remarks;
 }
