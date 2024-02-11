2ac529a19e5c97fcee802e7af5593cbb4dbad873
mai3373 <137670006+mai3373@users.noreply.github.com>
2024-02-11 23:59:26 +0900

    勤怠編集　バック実装　須藤

--- 2401teamdevelopment/src/main/java/com/example/demo/dto/AttendanceEditRequest.java
+++ 2401teamdevelopment/src/main/java/com/example/demo/dto/AttendanceEditRequest.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 /**
@@ -32,31 +34,37 @@ public class AttendanceEditRequest implements Serializable {
 	/**
 	 * 出勤日
 	 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "出勤日を入力してください")
 	private LocalDate going_date;
 	/**
 	 * 出勤時間
 	 */
+	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull(message = "出勤時間を入力してください")
 	private LocalTime going_time;
 	/**
 	 * 退勤日
 	 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "退勤日を入力してください")
 	private LocalDate leaving_date;
 	/**
 	 * 退勤時間
 	 */
+	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull(message = "退勤時間を入力してください")
 	private LocalTime leaving_time;
 	/**
 	 * 稼働時間
 	 */
+	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull(message = "稼働時間を入力してください")
 	private LocalTime working_time;
 	/**
 	 * 休憩時間
 	 */
+	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull(message = "休憩時間を入力してください")
 	private LocalTime break_time;
 	/**
