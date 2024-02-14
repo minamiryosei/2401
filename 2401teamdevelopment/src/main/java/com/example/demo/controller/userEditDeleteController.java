7a862a26fac3264f7059233a30873b2e02a7749b
rui-1017 <rui1017t@gmail.com>
2024-02-15 01:07:42 +0900

    controller

---
+++ 2401teamdevelopment/src/main/java/com/example/demo/controller/userEditDeleteController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dto.UserRequest;
+import com.example.demo.dto.UserUpdateRequest;
+import com.example.demo.entity.UserEntity;
+import com.example.demo.service.UserService;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+
+〜〜〜〜〜〜〜
+上記は省略
+〜〜〜〜〜〜〜
+
+  /**
+   * ユーザー編集画面を表示
+   * @param id 表示するユーザーID
+   * @param model Model
+   * @return ユーザー編集画面
+   */
+  @GetMapping("/user/{id}/edit")
+  public String userEdit(@PathVariable Integer id, Model model) {
+    UserEntity user = userService.findById(id);
+    UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
+    userUpdateRequest.setId(user.getId());
+    userUpdateRequest.setName(user.getName());
+    userUpdateRequest.setPhone(user.getPhone());
+    userUpdateRequest.setAddress(user.getAddress());
+    model.addAttribute("userUpdateRequest", userUpdateRequest);
+    return "user/edit";
+  }
+  /**
+   * ユーザー更新
+   * @param userRequest リクエストデータ
+   * @param model Model
+   * @return ユーザー情報詳細画面
+   */
+  @RequestMapping("/user/update")
+  public String userUpdate(@Validated @ModelAttribute UserUpdateRequest userUpdateRequest, BindingResult result, Model model) {
+    if (result.hasErrors()) {
+      List<String> errorList = new ArrayList<String>();
+      for (ObjectError error : result.getAllErrors()) {
+        errorList.add(error.getDefaultMessage());
+      }
+      model.addAttribute("validationError", errorList);
+      return "user/edit";
+    }
+    // ユーザー情報の更新
+    userService.update(userUpdateRequest);
+    return String.format("redirect:/user/%d", userUpdateRequest.getId());
+  }
+}
