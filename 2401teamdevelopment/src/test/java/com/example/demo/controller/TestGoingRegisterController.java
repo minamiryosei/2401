be8351563ded668761116eb3c7c32979b01fa6a3
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-02-23 13:19:05 +0900

    GoingRegisterControllerテストコードサンプル

---
+++ 2401teamdevelopment/src/test/java/com/example/demo/controller/TestGoingRegisterController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.service.GoingRegisterservice;
+
+@WebMvcTest(GoingRegisterController.class)
+class TestGoingRegisterController {
+
+	@MockBean
+	GoingRegisterservice goingRegisterservice;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void successfullyDisplayAdd() throws Exception {
+		mockMvc.perform(get("/goingRegister"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("goingRegisterrequest"))
+		.andExpect(view().name("GoingRegister"));
+	}
+
+
+}
