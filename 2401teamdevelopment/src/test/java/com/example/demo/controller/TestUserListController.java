4fd57a7fd3287c9cdbdb90cce565c09bd1614991
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-02-23 13:48:50 +0900

    UserListControllerテストコードサンプル

---
+++ 2401teamdevelopment/src/test/java/com/example/demo/controller/TestUserListController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.entity.UserListEntity;
+import com.example.demo.service.UserListService;
+
+@WebMvcTest(UserListController.class)
+class TestUserListController{
+
+	@MockBean
+	UserListService userListService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	/**
+	 * 【正常系】GETリクエストが正常に処理され、userList画面が表示されることを検証するテストケース
+	 * @throws Exception
+	 */
+	@Test
+	public void successfullyDisplayAdd() throws Exception {
+		List<UserListEntity> userList = new ArrayList<UserListEntity>();
+		when(userListService.searchAll()).thenReturn(userList);
+		mockMvc.perform(get("/userList"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("userList"))
+		.andExpect(view().name("userList"));
+	}
+}
