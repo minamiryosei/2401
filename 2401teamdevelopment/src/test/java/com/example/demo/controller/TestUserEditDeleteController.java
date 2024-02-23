10cbd1f1d84532403e32b9348e5e89290d8ab064
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-02-23 13:36:20 +0900

    UserEditDeleteControllerテストコードサンプル

---
+++ 2401teamdevelopment/src/test/java/com/example/demo/controller/TestUserEditDeleteController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.entity.userEditDelete;
+import com.example.demo.service.userEditDeleteService;
+
+@WebMvcTest(userEditDeleteController.class)
+class TestUserEditDeleteController{
+
+	@MockBean
+	userEditDeleteService userEditDeleteService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void successfullyDisplayAdd() throws Exception {
+		userEditDelete user = new userEditDelete();
+		user.setId((long) 1);
+		user.setName("name");
+		user.setFurigana("フリガナ");
+		user.setMail("sample@example.co.jp");
+		user.setPassword("Pass");
+		when(userEditDeleteService.findById((long) 1)).thenReturn(user);
+		mockMvc.perform(get("/userEditDelete/1/edit"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("userUpdateRequest"))
+		.andExpect(view().name("userEditDelete"));
+	}
+
+}
