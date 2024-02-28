c02e96e1d55fee44fda13e5694ff18dc6d2697ca
yutawatanabe0712 <yutaoneok0712@icloud.com>
2024-02-28 10:50:15 +0900

    AttendanceEditControllerテストケースの追加
    successfullyUpdateの追加

--- 2401teamdevelopment/src/test/java/com/example/demo/controller/AttendanceEditControllerTest.java
+++ 2401teamdevelopment/src/test/java/com/example/demo/controller/AttendanceEditControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo.controller.AttendanceEditController;
+import com.example.demo.dto.AttendanceEditRequest;
 import com.example.demo.entity.AttendanceEditEntity;
 import com.example.demo.service.AttendanceEditService;
 
@@ -31,7 +31,7 @@ class TestAttendanceEdit {
 	 * @throws Exception
 	 */
 	@Test
-	public void successfullyAttendanceEdit() throws Exception {
+	public void successfullyDisplayAdd() throws Exception {
 		AttendanceEditEntity attendanceEdit = new AttendanceEditEntity();
 		attendanceEdit.setAttendance_id(1);
 		attendanceEdit.setUser_id(1);
@@ -50,4 +50,28 @@ class TestAttendanceEdit {
 				.andExpect(model().attributeExists("AttendanceEditRequest"))
 				.andExpect(view().name("attendanceEdit"));
 	}
+
+	/**
+	 * 【正常系】POSTリクエストが正常に処理され、redirect:/attendanceListがreturnされることを検証するテストケース
+	 * @throws Exception
+	 */
+	@Test
+	public void successfullyUpdate() throws Exception{
+		AttendanceEditRequest attendanceEditRequest = new AttendanceEditRequest();
+		attendanceEditRequest.setAttendance_id(1);
+		attendanceEditRequest.setUser_id(1);
+		attendanceEditRequest.setStatus("status");
+		attendanceEditRequest.setGoing_date(LocalDate.of(2024, 10, 10));
+		attendanceEditRequest.setGoing_time(LocalTime.of(11, 30));
+		attendanceEditRequest.setLeaving_date(LocalDate.of(2024, 10, 10));
+		attendanceEditRequest.setLeaving_time(LocalTime.of(15, 30));
+		attendanceEditRequest.setWorking_time(LocalTime.of(8, 00));
+		attendanceEditRequest.setBreak_time(LocalTime.of(1, 0));
+		attendanceEditRequest.setEdit_reason("reason");
+		attendanceEditRequest.setRemarks("remarks");
+		mockMvc.perform((post("/attendanceEdit/update")).flashAttr("attendanceEditRequest", attendanceEditRequest))
+		.andExpect(model().hasNoErrors())
+		.andExpect(view().name("redirect:/attendanceList"));
+	}
+
 	}
